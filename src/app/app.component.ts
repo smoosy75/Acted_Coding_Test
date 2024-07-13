b2315a23e432d5e2537293681c862b58ad5a705d
Mustapha Said <mustaphasaid@Mustaphas-MacBook-Air.local>
2024-07-13 11:52:08 +0200

    added button toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { WeatherComponent } from './weather/weather.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [WeatherComponent],
+  imports: [WeatherComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'weatherAppTest';
+  showWeather = false;
+
+  toggleWeather() {
+    this.showWeather = !this.showWeather;
+  }
 }
